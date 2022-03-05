d67fe1595535e6bfb506c303815fa70c8aa89c56
Taishi-Kodai <rui.4019.fc@icloud.com>
2022-03-05 13:08:45 +0900

    [add]記事詳細画面

--- app/Post.php
+++ app/Post.php
@@ -6,14 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-<<<<<<< HEAD
     public function getPaginateByLimit(int $limit_count = 5)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
-=======
-   //
-}
-
->>>>>>> master
