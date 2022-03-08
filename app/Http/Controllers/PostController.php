e5b85f6bbe2811dfdf2ee8969a64f1d1990fbf08
Taishi-Kodai <rui.4019.fc@icloud.com>
2022-03-08 16:01:25 +0900

    [add]ブログの編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,10 +22,23 @@ class PostController extends Controller
        return view('create');
    }
    
-   public function store(Post $post, PostRequest $request)
+   public function store(PostRequest $request, Post $post)
    {
        $input = $request['post'];
        $post->fill($input)->save();
+       return redirect('/posts/' . $post->id);
+   }
+   
+   public function edit(Post $post)
+   {
+       return view('edit')->with(['post' => $post]);
+   }
+   
+   public function update(PostRequest $request, Post $post)
+   {
+       $input_post = $request['post'];
+       $post->fill($input_post)->save();
+       
        return redirect('/posts/' . $post->id);
    }
 }
